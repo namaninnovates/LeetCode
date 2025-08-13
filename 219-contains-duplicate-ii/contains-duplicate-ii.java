097dc54656676341601cc5f34d5e7aa5f0439d8d
Naman Gupta <157508788+namaninnovates@users.noreply.github.com>
2025-08-14 03:06:35 +0530

    Time: 22 ms (52.22%) | Memory: 60.1 MB (27.55%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,28 +1,15 @@
-import java.util.*;
-
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // Create a HashMap to store number -> last seen index
-        Map<Integer, Integer> lastIndex = new HashMap<>();
+        Map<Integer, Integer> seen = new HashMap<>();
 
-        // Loop through all elements in nums
         for (int i = 0; i < nums.length; i++) {
-            // If we've seen nums[i] before
-            if (lastIndex.containsKey(nums[i])) {
-                int prevIndex = lastIndex.get(nums[i]); // where it was last seen
-                int distance = i - prevIndex; // how far apart
-
-                // If distance <= k, we found a nearby duplicate
-                if (distance <= k) {
-                    return true; // no need to check further
-                }
+            int val = nums[i];
+            if (seen.containsKey(val) && i - seen.get(val) <= k) {
+                return true;
             }
-
-            // Update the last seen index for nums[i]
-            lastIndex.put(nums[i], i);
+            seen.put(val, i);
         }
 
-        // If loop finishes, no nearby duplicates found
-        return false;
+        return false;        
     }
 }
